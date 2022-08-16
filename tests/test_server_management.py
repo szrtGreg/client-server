f0a685e9f8a714356dbb76455b50df6a1b695964
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2022-08-16 11:55:01 +0200

    test done

---
+++ tests/test_server_management.py
@@ -0,0 +1,22 @@
+import pytest
+from unittest.mock import Mock
+import sys
+sys.path.append(r"c:\newWork\client-server")
+
+from server_management import ServerManagement
+from data_base import DataBase
+
+
+
+def test_show_server_info():
+    server_management = ServerManagement()
+    output = server_management.show_server_info()
+    assert output == ('info', 'server v.2')
+
+
+def test_show_server_uptime():
+    server_management = ServerManagement()
+    server_management.server_uptime = Mock(return_value='00:00:01')
+    output = server_management.show_server_uptime()
+    assert output == ('uptime', '00:00:01')
+
